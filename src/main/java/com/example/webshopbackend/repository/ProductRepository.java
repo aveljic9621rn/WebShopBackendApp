55adc071a029be4d10e4d7f31f0d4f00aa3d86ea
aveljic9621rn <aveljic9621rn@raf.rs>
2023-11-16 00:26:13 +0100

    Bug fix for ProductRepository

--- src/main/java/com/example/webshopbackend/repository/ProductRepository.java
+++ src/main/java/com/example/webshopbackend/repository/ProductRepository.java
@@ -2,9 +2,10 @@ package com.example.webshopbackend.repository;
 
 import com.example.webshopbackend.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
     Product findProductByProductID(String ID);
     Optional<Product> findProductByName(String name);
